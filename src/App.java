4ba4322ee516ca10dad0a5ec7023a7dd67427a64
sga-encoder <sebastian.garzon54795@ucaldas.edu.co>
2024-11-05 12:14:56 -0500

    Push autom├ítico el 2024-11-05 12:14:56

---
+++ src/App.java
@@ -0,0 +1,9 @@
+// import 
+import utilities.Printer;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Printer printer = new Printer();
+        printer.template( "@c2hola", false); 
+    }
+}
